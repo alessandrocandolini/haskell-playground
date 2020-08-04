f8cba1cc6ed468e9a004c544a7d70894ac3960eb
Alessandro Candolini <ac@spotli.me>
2020-08-04 10:03:24 +0100

    Test

--- chap1/test/Spec.hs
+++ chap1/test/Spec.hs
@@ -13,9 +13,9 @@ tests :: TestTree
 tests = testGroup "Tests" 
    [ 
      testCase "if title is empty, return error" $ validate (Todo "" 2) @?= EmptyTitle,
-		  testProperty "if title is not empty, don't return empty title error" $ \x -> x /= "" ==> validate (Todo x 2) /= EmptyTitle
+     testProperty "if title is not empty, don't return empty title error" $ \x -> x /= "" ==> validate (Todo x 2) /= EmptyTitle
 
-  ]
+   ]
 
 
 -- stack test --ta "-p <pattern>" 
