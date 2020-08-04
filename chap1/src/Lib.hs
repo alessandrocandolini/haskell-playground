109ff3a46d4327f96204651fde0acebd04a95e36
Alessandro Candolini <ac@spotli.me>
2020-08-04 02:10:42 +0100

    tasty empty setup

--- chap1/src/Lib.hs
+++ chap1/src/Lib.hs
@@ -1,2 +1,14 @@
 module Lib where
+import qualified Data.Text as T
+-- data Todo = forall a . Ord a => Todo String a 
+data Todo a = Todo T.Text a 
+
+data Result = EmptyTitle | PastDueDate | Success 
+
+x :: Todo Integer
+x = Todo "hello" 5
+
+f :: Ord a => Todo a -> Result
+f (Todo "" _) = EmptyTitle
+f (Todo _  _) = Success
 
