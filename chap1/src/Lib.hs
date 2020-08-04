8cf040e511e116ef125bff0c285a06323dbd351f
Alessandro Candolini <ac@spotli.me>
2020-08-04 02:50:17 +0100

    Simple tests

--- chap1/src/Lib.hs
+++ chap1/src/Lib.hs
@@ -1,14 +1,14 @@
 module Lib where
 import qualified Data.Text as T
 -- data Todo = forall a . Ord a => Todo String a 
-data Todo a = Todo T.Text a 
+data Todo a = Todo T.Text a deriving (Show,Eq)
 
-data Result = EmptyTitle | PastDueDate | Success 
+data Result = EmptyTitle | PastDueDate | Success deriving (Show,Eq)
 
 x :: Todo Integer
 x = Todo "hello" 5
 
-f :: Ord a => Todo a -> Result
-f (Todo "" _) = EmptyTitle
-f (Todo _  _) = Success
+validate :: Ord a => Todo a -> Result
+validate (Todo "" _) = EmptyTitle
+validate (Todo _  _) = Success
 
--- chap1/test/Spec.hs
+++ chap1/test/Spec.hs
@@ -1,7 +1,21 @@
+module Main where
+
 import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Data.Text.Arbitrary
+import Lib
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" []
+tests = testGroup "Tests" 
+   [ 
+     testCase "if title is empty, return error" $ validate (Todo "" 2) @?= EmptyTitle,
+		  testProperty "if title is not empty, don't return empty title error" $ \x -> x /= "" ==> validate (Todo x 2) /= EmptyTitle
+
+  ]
+
+
+-- stack test --ta "-p <pattern>" 
