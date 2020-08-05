f13433564bc4e0aaf29d5abc3b56dabc771d0c18
Alessandro Candolini <ac@spotli.me>
2020-08-05 02:04:14 +0100

    more pbt

--- chap1/src/Lib.hs
+++ chap1/src/Lib.hs
@@ -8,7 +8,8 @@ data Result = EmptyTitle | PastDueDate | Success deriving (Show,Eq)
 x :: Todo Integer
 x = Todo "hello" 5
 
-validate :: Ord a => Todo a -> Result
-validate (Todo "" _) = EmptyTitle
-validate (Todo _  _) = Success
+validate :: Ord a => Todo a -> a -> Result
+validate (Todo "" _ ) _ = EmptyTitle
+validate (Todo _  d ) current | d <=current = PastDueDate
+validate _ _ = Success
 
--- chap1/test/Spec.hs
+++ chap1/test/Spec.hs
@@ -1,5 +1,4 @@
 module Main where
-
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
@@ -12,9 +11,14 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" 
    [ 
-     testCase "if title is empty, return error" $ validate (Todo "" 2) @?= EmptyTitle,
-     testProperty "if title is not empty, don't return empty title error" $ \x -> x /= "" ==> validate (Todo x 2) /= EmptyTitle
+     testCase "if title is empty, return error" $ 
+             validate (Todo "" 2) 3 @?= EmptyTitle,
 
+     testProperty "never return EmptyTitle error when the title is not the empty string " $ 
+             \ x -> x /= "" ==> validate( Todo x 3) 2 /= EmptyTitle,
+
+     testProperty "if return EmptyTitle then title must be empty when the title is not the empty string " $ 
+             \x -> validate (Todo x 2 ) 3 /= EmptyTitle  ==> x /= ""
    ]
 
 
