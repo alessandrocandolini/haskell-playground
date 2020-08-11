095fa05e352f32b93dc8443d4232ccf2a8b615ff
Alessandro Candolini <ac@spotli.me>
2020-08-11 22:15:45 +0100

    Cleanup

--- chap1/src/Lib.hs
+++ chap1/src/Lib.hs
@@ -1,12 +1,10 @@
 module Lib where
 import Data.Text
+
 -- data Todo = forall a . Ord a => Todo String a 
 data Todo a = Todo Text a deriving (Show,Eq)
 
 data Result = EmptyTitle | PastDueDate | Success deriving (Show,Eq)
-
-x :: Todo Integer
-x = Todo "hello" 5
 
 validate :: Ord a => Todo a -> a -> Result
 validate (Todo "" _ ) _ = EmptyTitle
