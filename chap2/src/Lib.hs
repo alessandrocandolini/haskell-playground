981c3d74231a4b26054197b589c54b5ff775bdac
Alessandro Candolini <ac@spotli.me>
2020-08-16 18:27:34 +0100

    Some PBT

--- chap2/src/Lib.hs
+++ chap2/src/Lib.hs
@@ -5,15 +5,16 @@ myIdentity a = a
 
 data Option a = None | Some a  deriving (Eq,Show)
 
-data MyList a = Empty | NonEmpty a (MyList a) deriving (Eq,Show)
+data MyList a = MyEmpty | MyNonEmpty a (MyList a) deriving (Eq,Show)
 
 mymap :: (a -> b) -> MyList a -> MyList b 
-mymap _ Empty = Empty
-mymap f (NonEmpty head tail) = NonEmpty (f head) (mymap f tail)
-
+mymap _ MyEmpty = MyEmpty
+mymap f (MyNonEmpty head tail) = MyNonEmpty (f head) (mymap f tail)
 
 myfilter :: (a -> Bool) -> MyList a -> MyList a
-myfilter p Empty = Empty
-myfilter p (NonEmpty head tail) | p head = NonEmpty (p head) (myfilter p tail)
-myfilter p (NonEmpty head tail) = myfilter p tail
+myfilter _ MyEmpty = MyEmpty
+myfilter p (MyNonEmpty head tail) 
+  | p head = MyNonEmpty head (myfilter p tail) 
+  | otherwise =  myfilter p tail
+
 
--- chap2/test/Spec.hs
+++ chap2/test/Spec.hs
@@ -6,6 +6,11 @@ import Lib
 prop_additionCommutative :: Int -> Int -> Bool
 prop_additionCommutative a b = a + b == b + a
 
-prop_mapIdentityLeaveTheListUnchanged :: MyList Int -> Bool
-prop_mapIdentityLeaveTheListUnchanged l = mymap myIdentity l == l 
+-- prop_mapIdentityLeaveTheListUnchanged :: MyList Int -> Bool
+-- prop_mapIdentityLeaveTheListUnchanged l = mymap myIdentity l == l 
 
+prop_filter_idempotent:: Fun Int Bool -> [Int] -> Bool
+prop_filter_idempotent (Fun _ p) l = filter p (filter p l) == filter p l  
+
+prop_filter_empty:: Fun Int Bool -> [Int] -> Bool
+prop_filter_empty (Fun _ p) l = filter p (filter (not.p)  l) == []
