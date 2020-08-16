7e6e7f007c3b530974a0a5545dc80001e2deaa8e
Alessandro Candolini <ac@spotli.me>
2020-08-16 18:35:42 +0100

    MOre PBT

--- chap2/test/Spec.hs
+++ chap2/test/Spec.hs
@@ -14,3 +14,9 @@ prop_filter_idempotent (Fun _ p) l = filter p (filter p l) == filter p l
 
 prop_filter_empty:: Fun Int Bool -> [Int] -> Bool
 prop_filter_empty (Fun _ p) l = filter p (filter (not.p)  l) == []
+
+prop_map_identity:: [Int] -> Bool
+prop_map_identity l = map myIdentity l == l 
+
+prop_map_length_preserving :: Fun Int String -> [Int] -> Bool
+prop_map_length_preserving (Fun _ f) l = length (map f l) == length l
