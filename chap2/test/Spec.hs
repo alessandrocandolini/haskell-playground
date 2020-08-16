43f23c7324ab070c91a5fb57282b19913158cdd1
Alessandro Candolini <ac@spotli.me>
2020-08-16 15:19:00 +0100

    chap2 WIP

--- chap2/test/Spec.hs
+++ chap2/test/Spec.hs
@@ -1,2 +1,11 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Spec where
+import Test.Tasty
+import Test.Tasty.QuickCheck 
+import Lib
+
+prop_additionCommutative :: Int -> Int -> Bool
+prop_additionCommutative a b = a + b == b + a
+
+prop_mapIdentityLeaveTheListUnchanged :: MyList Int -> Bool
+prop_mapIdentityLeaveTheListUnchanged l = mymap myIdentity l == l 
+
