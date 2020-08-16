ce7c781ca508d6c6d7ac916747060136b35c6b0c
Alessandro Candolini <ac@spotli.me>
2020-08-16 18:45:16 +0100

    Rmeoved template

--- chap2/test/Spec.hs
+++ chap2/test/Spec.hs
@@ -3,9 +3,6 @@ import Test.Tasty
 import Test.Tasty.QuickCheck 
 import Lib
 
-prop_additionCommutative :: Int -> Int -> Bool
-prop_additionCommutative a b = a + b == b + a
-
 -- prop_mapIdentityLeaveTheListUnchanged :: MyList Int -> Bool
 -- prop_mapIdentityLeaveTheListUnchanged l = mymap myIdentity l == l 
 
