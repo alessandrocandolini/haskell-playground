68d3af9ab66c3345940b4e6c6bd2b6baf343bc45
Alessandro Candolini <ac@spotli.me>
2020-11-03 02:41:38 +0000

    Working with the full program

--- chap4/src/Lib.hs
+++ chap4/src/Lib.hs
@@ -1,25 +1,97 @@
 module Lib where
+import Data.Char
+import Data.Either.Extra
+import Data.Time
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+newtype NonEmptyString = NonEmptyString String deriving (Show, Eq)
+safeNonEmptyString :: String -> Maybe NonEmptyString
+safeNonEmptyString s | s == "" = Nothing 
+                     | otherwise = Just (NonEmptyString s)
 
-newtype TodoTitle = TodoTitle String deriving (Eq,Show)
+newtype TodoTitle = TodoTitle NonEmptyString deriving (Eq,Show)
 
 data Todo a = Todo {
    title :: TodoTitle, 
    dueDate :: Maybe a
    } deriving (Eq,Show)
 
+
+
+{--
+  A simple approach (runtime checks, no trim, monolytic function, ad hoc response type, etc) 
+--}   
+
 data AddTodoResult a = Notitle | PastDueDate | Success (Todo a) deriving (Eq,Show)
 
--- addTodo :: Ord a => String -> Maybe a -> a -> AddTodoResult a
-addTodo "" _ _ = Notitle 
-addTodo t a b | isDatePast a b = PastDueDate 
-              | otherwise =  Success todo where 
-                   todo = Todo (TodoTitle t) (Just b) 
+simpleAddTodo :: Ord a => String -> Maybe a -> a -> AddTodoResult a
+simpleAddTodo "" _ _ = Notitle 
+simpleAddTodo t a b | isDatePast a b = PastDueDate 
+              | otherwise =  Success todo 
+              where 
+                   todo = Todo (TodoTitle (NonEmptyString t)) (Just b)  -- type-unsafe runtime promotion of string to nonemptystring 
+                   isDatePast Nothing _ = False
+                   isDatePast (Just x) y = x < y 
 
 
-isDatePast :: Ord a => Maybe a -> a -> Bool                   
-isDatePast Nothing _ = False
-isDatePast (Just a) b = a < b 
+{--
+  A bit more compositionality  (still not ideal)
+--}   
+
+wrongTrim :: String -> String 
+wrongTrim = filter (/= ' ')
+
+myDropWhile             :: (a -> Bool) -> [a] -> [a]
+myDropWhile _ []        =  []
+myDropWhile p (x:xs)
+            | p x       =  myDropWhile p xs
+            | otherwise =  (x:xs)
+
+inefficientTrim = trimEnd . trimBeginning 
+     where
+       trimBeginning = myDropWhile isSpace
+       trimEnd                          = reverse . trimBeginning . reverse                       
+
+trim = inefficientTrim
+
+
+data AddTodoValidation = ErrorTitle| ErrorDueDate deriving (Eq,Show)
+type TodoValidationResult a = Either AddTodoValidation a 
+
+basicCheckTitle    :: String -> TodoValidationResult NonEmptyString 
+-- basicCheckTitle "" = Left ErrorTitle 
+-- basicCheckTitle s  = Right s 
+basicCheckTitle =  (maybeToEither ErrorTitle) . safeNonEmptyString
+
+checkTitle = basicCheckTitle . trim
+
+checkDueDate :: Ord a => Maybe a -> a -> TodoValidationResult (Maybe a)
+checkDueDate (Just a) d | a < d = Left ErrorDueDate
+                        | otherwise = Right (Just a)
+
+addTodo2 :: Ord a => String -> Maybe a -> a -> TodoValidationResult (Todo a)
+addTodo2 t a b = do 
+   t' <- checkTitle t
+   d' <- checkDueDate a b 
+   return $ Todo (TodoTitle t') d'
+
+
+
+-- Full program 
+
+parseDate :: String -> Maybe ZonedTime  
+parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
+
+business :: String -> String -> IO String 
+business rawTitle rawDate = do 
+   now <- getZonedTime
+   return "hello" 
+
+program :: IO ()
+program = do 
+  putStrLn "insert title:"
+  rawTitle <- getLine 
+  putStrLn "insert date:" 
+  rawDate <- getLine
+  result <- business rawTitle rawDate 
+  putStrLn result 
 
--- chap4/test/LibSpec.hs
+++ chap4/test/LibSpec.hs
@@ -6,15 +6,57 @@ import Lib
 
 spec :: Spec
 spec = do 
-  describe "addTodo" $ do
-    prop "returns no title whenever the title is empty" $ 
-      \a -> \b -> (addTodo "" (a::Maybe Int) (b::Int)) == Notitle
+  describe "LibSpec" $ do
+    describe "simpleAddTodo" $ do
+      prop "returns no title whenever the title is empty" $ 
+        \a -> \b -> (simpleAddTodo "" (a::Maybe Int) (b::Int)) == Notitle
 
-    prop "never returns PastDueDate when the date is not present" $
-      \t -> \a -> (addTodo t Nothing (a::Int)) /= PastDueDate
+      prop "never returns PastDueDate when the date is not present" $
+        \t -> \a -> (simpleAddTodo t Nothing (a::Int)) /= PastDueDate
     
-    prop "never returns PastDueDate when the date is in the future present" $
-      \t -> \a -> (addTodo t (Just (a+1)) (a::Int)) /= PastDueDate
+      prop "never returns PastDueDate when the date is in the future present" $
+        \t -> \a -> (simpleAddTodo t (Just (a+1)) (a::Int)) /= PastDueDate
 
-    it "true should be true" $
-      True `shouldBe` True
+    describe "wrongTrim" $ do 
+       it "should remove all spaces from a string without other characters" $
+         (wrongTrim "   ") `shouldBe` ""
+    
+       it "should leave empty string unchanged" $
+         (wrongTrim "") `shouldBe` ""
+    
+       it "should leave strings with characters unchanged" $
+         (wrongTrim "sss") `shouldBe` "sss"
+       
+       it "should remove empty spaces at the beginning and end of the string while leaving the rest of the string unchanged" $
+         (wrongTrim " sss  ") `shouldBe` "sss"
+       
+       it "should leave strings with characters and spaces in the middle unchanged (the test here says shouldNOTbe to show there is an error)" $
+         (wrongTrim "s ss") `shouldNotBe` "s ss"
+
+    describe "inefficientTrim" $ do 
+       it "should remove all spaces from a string without other characters" $
+         (inefficientTrim "   ") `shouldBe` ""
+    
+       it "should leave empty string unchanged" $
+         (inefficientTrim"") `shouldBe` ""
+    
+       it "should leave strings with characters unchanged" $
+         (inefficientTrim"sss") `shouldBe` "sss"
+       
+       it "should leave strings with characters and spaces in the middle unchanged, but strip those at the end" $
+         (inefficientTrim"  s s  s   ") `shouldBe` "s s  s"
+    
+    describe "myDropWhile" $ do 
+       it "should return empty list if the condtiion is never met" $
+         (myDropWhile even [2,4,6,7,9,11,12,13,14]) `shouldBe` [7,9,11,12,13,14]
+    
+
+
+
+    describe "test should work" $ do 
+       it "true should be true" $
+         True `shouldBe` True
+
+    describe "test should work" $ do 
+       it "true should be true" $
+         True `shouldBe` True
